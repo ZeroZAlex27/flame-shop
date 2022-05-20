05a685af957dd1dee0060b9476055b4b406085be
Aleksey <71120586+ZeroZAlex27@users.noreply.github.com>
2022-05-21 03:23:38 +0600

    Добавил страницу регистрации и авторизации

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Button, Card, Container, Form } from "react-bootstrap";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
-    return <div>Auth</div>;
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE;
+
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{ height: window.innerHeight - 54 }}
+        >
+            <Card style={{ width: 600 }} className="p-5">
+                <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control className="mt-3" placeholder="Email" />
+                    <Form.Control className="mt-3" placeholder="Пароль" />
+                    <div className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                        {isLogin ? (
+                            <div className="mb-3">
+                                Нет аккаунта?{" "}
+                                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь!</NavLink>
+                            </div>
+                        ) : (
+                            <div>
+                                Уже зарегистрированы? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        )}
+                        <Button variant={"outline-success"}>
+                            {isLogin ? "Войти" : "Создать аккаунт"}
+                        </Button>
+                    </div>
+                </Form>
+            </Card>
+        </Container>
+    );
 };
 
 export default Auth;
